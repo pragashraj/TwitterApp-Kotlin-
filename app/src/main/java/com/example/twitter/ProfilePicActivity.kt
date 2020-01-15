3ec558bb0eecf8011b589a344e76710fbe4166a9
S.Pragashraj <rajpragashraj30@gmail.com>
2020-01-15 11:34:07 +0530

    profile Pic Ui

---
+++ app/src/main/java/com/example/twitter/ProfilePicActivity.kt
@@ -0,0 +1,23 @@
+package com.example.twitter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class ProfilePicActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile_pic)
+    }
+
+    fun onChooseBtnClick(v:View)
+    {
+
+    }
+
+    fun onNextBtnClick(v:View)
+    {
+
+    }
+}
